df111c393506321a60d39517e9cbd44b0b08e42d
Doug McCorkle <doug@agsolver.com>
2015-04-19 15:09:27 -0500

    add back in the old code and remove the unneeded find scripts.

--- src/sysdefs.h
+++ src/sysdefs.h
@@ -1,20 +1,17 @@
 #ifndef SYSDEFS_H
 #define SYSDEFS_H
 
-#define NOMINMAX
-
 #include "logger.h"
 #include <vector>
 #include <string>
 #include <sstream>
 #include <map>
-//#include <boost\shared_ptr.hpp>
 #include <memory>
 using namespace std;
-//using namespace std::tr1;
 
 #include <glew.h>
 #ifdef _WIN32
+#define NOMINMAX
 #include <wglew.h>
 #include <gl/gl.h>
 #include <gl/glu.h>
